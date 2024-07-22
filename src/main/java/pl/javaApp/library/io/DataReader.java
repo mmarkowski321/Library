225cf4d55774c4469d85a420fdec1c2ec2ab2367
mmarkowski <markowskimilosz989@gmail.com>
2024-07-22 15:41:16 +0200

    added security for reading data

--- src/main/java/pl/javaApp/library/io/DataReader.java
+++ src/main/java/pl/javaApp/library/io/DataReader.java
@@ -1,14 +1,18 @@
 package pl.javaApp.library.io;
 
+import pl.javaApp.library.exceptions.InvalidDataException;
 import pl.javaApp.library.model.Book;
 import pl.javaApp.library.model.LibraryUser;
 import pl.javaApp.library.model.Magazine;
 
+import java.time.LocalDate;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class DataReader {
     Scanner sc = new Scanner(System.in);
-    private ConsolePrinter printer;
+    private final ConsolePrinter printer;
+    private final LocalDate localDate = LocalDate.now();
 
     public DataReader(ConsolePrinter printer) {
         this.printer = printer;
@@ -22,14 +26,15 @@ public class DataReader {
         printer.printLine("Wydawnictwo: ");
         String publisher = sc.nextLine();
         printer.printLine("ISBN: ");
-        String isbn = sc.nextLine();
+        String isbn = getISBN();
         printer.printLine("Rok wydania: ");
-        int releaseDate = getInt();
+        int releaseDate = getYear();
         printer.printLine("Ilość stron: ");
-        int pages = getInt();
+        int pages = getAmountOfPages();
 
         return new Book(title, publisher, releaseDate, pages, author, isbn);
     }
+
     public Magazine readAndCreateMagazine() {
         printer.printLine("Tytuł: ");
         String title = sc.nextLine();
@@ -37,36 +42,116 @@ public class DataReader {
         String publisher = sc.nextLine();
         printer.printLine("Język: ");
         String language = sc.nextLine();
-        printer.printLine("Rok wydania: ");
-        int year = getInt();
-        printer.printLine("Miesiąc: ");
-        int month = getInt();
-        printer.printLine("Dzień: ");
-        int day = getInt();
+        LocalDate date = getCorrectDate();
 
-        return new Magazine(title, publisher,year, language,  month, day);
+        return new Magazine(title, publisher, date.getYear(), language, date.getMonthValue(), date.getDayOfMonth());
     }
-    public LibraryUser createLibraryUser(){
-        printer.printLine("Wpisz imie");
+
+    public LibraryUser createLibraryUser() {
+        printer.printLine("Wpisz imie: ");
         String name = sc.nextLine();
-        printer.printLine("Wpisz nazwisko");
+        printer.printLine("Wpisz nazwisko: ");
         String lastname = sc.nextLine();
-        printer.printLine("Wpisz pesel");
-        String pesel = sc.nextLine();
-        return new LibraryUser(name,lastname,pesel);
+        printer.printLine("Wpisz pesel: ");
+        String pesel = getPesel();
+        return new LibraryUser(name, lastname, pesel);
     }
-    public int getInt(){
-        try{
-            return sc.nextInt();
-        }finally {
-            sc.nextLine();
+
+    private int getAmountOfPages() {
+        boolean correctAmountPages = true;
+        int pages = 0;
+        while (correctAmountPages){
+            pages = getInt();
+            if (pages <= 0){
+                System.out.println("Niepoprawna ilość stron, spróbuj ponownie");
+            }else{
+                correctAmountPages = false;
+            }
+        }
+        return pages;
+    }
+
+    private String getPesel() {
+        boolean correctPesel = true;
+        int lengthOfPesel = 11;
+        String pesel = null;
+        while (correctPesel){
+            pesel = getString();
+            if (pesel.length() != lengthOfPesel){
+                System.out.println("Niepoprawny pesel, spróbuj jeszcze raz");
+            }else{
+                correctPesel = false;
+            }
+        }
+        return pesel;
+    }
+
+    private LocalDate getCorrectDate() {
+        LocalDate date = null;
+        boolean correctDate = false;
+        while (!correctDate) {
+            printer.printLine("Rok wydania: ");
+            int year = getInt();
+            printer.printLine("Miesiąc: ");
+            int month = getInt();
+            printer.printLine("Dzień: ");
+            int day = getInt();
+
+            try {
+                date = LocalDate.of(year, month, day);
+                if (date.isAfter(localDate)) {
+                    System.out.println("Niepoprawna data, spróbuj ponownie.");
+                } else {
+                    correctDate = true;
+                }
+            } catch (Exception e) {
+                System.out.println("Niepoprawna data, spróbuj ponownie.");
+            }
+        }
+        return date;
+    }
+
+    private int getYear() {
+        while (true) {
+            int year = localDate.getYear();
+            int releaseDate = getInt();
+            if (releaseDate > year) {
+                System.out.println("Niepoprawny rok wydania, spróbuj jeszcze raz.");
+            } else {
+                return releaseDate;
+            }
         }
     }
-    public void close(){
-        sc.close();
-    }
-    public String getString(){
-        return sc.nextLine();
+
+    private String getISBN() {
+        while (true) {
+            String ISBN = getString();
+            if (ISBN.length() != 13) {
+                System.out.println("Niepoprawny ISBN, spróbuj jeszcze raz.");
+            } else {
+                return ISBN;
+            }
+        }
     }
 
+    public int getInt() throws InputMismatchException {
+        while (true) {
+            try {
+                return sc.nextInt();
+            } catch (InputMismatchException e) {
+                sc.next();
+                System.out.println("Niepoprawna wartość, spróbuj ponownie.");
+            } finally {
+                sc.nextLine();
+            }
+        }
+    }
+
+    public void close() {
+        sc.close();
+    }
+
+    public String getString() {
+        return sc.nextLine();
+    }
 }
